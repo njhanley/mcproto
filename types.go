bd327259b8a181a04107492c5f0ecc98e901cbc1
njhanley <njhanley@users.noreply.github.com>
2018-10-10 18:11:43 -0400

    merge {packets,varint}.go with types.go

--- types.go
+++ types.go
@@ -3,14 +3,84 @@ package protocol
 import (
 	"encoding/binary"
 	"math"
+	"math/bits"
 
 	"github.com/pkg/errors"
+)
+
+const (
+	cbits = 7          // bits per chunk
+	msb   = 1 << cbits // most significant bit
+	cmask = msb - 1    // chunk mask
+
+	maxIntBytes  = 5
+	maxLongBytes = 10
 )
 
 var (
 	errBufTooSmall   = errors.New("buf too small")
 	errValueTooLarge = errors.New("value too large")
 )
+
+func getVarN(buf []byte, maxBytes int) (v uint64, n int, err error) {
+	for i := 0; i < len(buf) && i < maxBytes; i++ {
+		v |= uint64(buf[i]&cmask) << uint(i*cbits)
+		if buf[i]&msb == 0 {
+			return v, i + 1, nil
+		}
+	}
+	if len(buf) < maxBytes {
+		return 0, len(buf), errors.WithStack(errBufTooSmall)
+	}
+	return 0, maxBytes, errors.WithStack(errValueTooLarge)
+}
+
+func getVarInt(buf []byte) (v int32, n int, err error) {
+	_v, n, err := getVarN(buf, maxIntBytes)
+	return int32(_v), n, err
+}
+
+func getVarLong(buf []byte) (v int64, n int, err error) {
+	_v, n, err := getVarN(buf, maxLongBytes)
+	return int64(_v), n, err
+}
+
+func putVarN(buf []byte, v uint64, maxBytes int) (int, error) {
+	for i := 0; i < len(buf) && i < maxBytes; i++ {
+		if v&^cmask == 0 {
+			buf[i] = byte(v)
+			return i + 1, nil
+		}
+		buf[i] = byte(v | msb)
+		v >>= cbits
+	}
+	if len(buf) < maxBytes {
+		return len(buf), errors.WithStack(errBufTooSmall)
+	}
+	return maxBytes, errors.WithStack(errValueTooLarge)
+}
+
+func putVarInt(buf []byte, v int32) (int, error) {
+	// convert to uint32 before uint64 to avoid sign extension
+	return putVarN(buf, uint64(uint32(v)), maxIntBytes)
+}
+
+func putVarLong(buf []byte, v int64) (int, error) {
+	return putVarN(buf, uint64(v), maxLongBytes)
+}
+
+func lenVarN(v uint64) int {
+	return 1 + ((bits.Len64(v) - 1) / cbits)
+}
+
+func lenVarInt(v int32) int {
+	// convert to uint32 before uint64 to avoid sign extension
+	return lenVarN(uint64(uint32(v)))
+}
+
+func lenVarLong(v int64) int {
+	return lenVarN(uint64(v))
+}
 
 func getString(buf []byte) (s string, n int, err error) {
 	length, m, err := getVarInt(buf)
@@ -86,4 +156,54 @@ func putPosition(buf []byte, p position) (int, error) {
 	v |= uint64(p.z) << 38 >> 38
 	binary.BigEndian.PutUint64(buf, v)
 	return positionLen, nil
+}
+
+type packet struct {
+	id   int32
+	data []byte
+}
+
+func getPacket(buf []byte) (p packet, n int, err error) {
+	length, m, err := getVarInt(buf)
+	if n += m; err != nil {
+		return p, n, err
+	}
+
+	id, m, err := getVarInt(buf[n:])
+	if n += m; err != nil {
+		return p, n, err
+	}
+
+	l := int(length) - m
+	if len(buf) < n+l {
+		return p, n, errors.WithStack(errBufTooSmall)
+	}
+	data := make([]byte, l)
+	n += copy(data, buf[n:n+l])
+
+	return packet{id, data}, n, nil
+}
+
+func putPacket(buf []byte, p packet) (n int, err error) {
+	length := lenVarInt(p.id) + len(p.data)
+	if length > math.MaxInt32 {
+		return n, errors.WithStack(errValueTooLarge)
+	}
+
+	m, err := putVarInt(buf, int32(length))
+	if n += m; err != nil {
+		return n, err
+	}
+
+	m, err = putVarInt(buf[n:], p.id)
+	if n += m; err != nil {
+		return n, err
+	}
+
+	m = copy(buf[n:], p.data)
+	if n += m; m < len(p.data) {
+		return n, errors.WithStack(errBufTooSmall)
+	}
+
+	return n, nil
 }
--- types_test.go
+++ types_test.go
@@ -2,38 +2,251 @@ package protocol
 
 import (
 	"bytes"
+	"math"
 	"math/rand"
+	"reflect"
 	"testing"
 
 	"github.com/pkg/errors"
 )
 
 var (
-	stringCases = []struct {
+	varIntCases = []struct {
 		bytes  []byte
-		value  string
+		value  int32
 		length int
 		err    error
 	}{
-		{[]byte{0x00}, "", 1, nil}, // empty string
-		{[]byte{0x0d, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21}, "Hello, world!", 14, nil},    // ASCII string
-		{[]byte{0x0e, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0xe4, 0xb8, 0x96, 0xe7, 0x95, 0x8c, 0x21}, "Hello, 世界!", 15, nil}, // UTF-8 string
-		{[]byte{}, "", 0, errBufTooSmall},                   // empty buffer
-		{[]byte{0x01}, "", 1, errBufTooSmall},               // incomplete buffer
-		{[]byte{0xff, 0xff, 0x02}, "", 3, errValueTooLarge}, // string length > math.MaxInt16
+		// valid
+		{[]byte{0x00}, 0, 1, nil},                                     // zero
+		{[]byte{0x01}, 1, 1, nil},                                     // one
+		{[]byte{0x7f}, 127, 1, nil},                                   // largest value of length 1
+		{[]byte{0x80, 0x01}, 128, 2, nil},                             // smallest value of length 2
+		{[]byte{0xff, 0xff, 0xff, 0xff, 0x07}, math.MaxInt32, 5, nil}, // largest positive value
+		{[]byte{0xff, 0xff, 0xff, 0xff, 0x0f}, -1, 5, nil},            // negative one
+		{[]byte{0x80, 0x80, 0x80, 0x80, 0x08}, math.MinInt32, 5, nil}, // largest negative value
+		// invalid
+		{[]byte{}, 0, 0, errBufTooSmall},                                     // empty buffer
+		{[]byte{0xff}, 0, 1, errBufTooSmall},                                 // incomplete buffer
+		{[]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0x0f}, 0, 5, errValueTooLarge}, // value overflow
+		{[]byte{0xff, 0xff, 0xff, 0xff, 0xff}, 0, 5, errValueTooLarge},       // incomplete buffer with value overflow
 	}
-	positionCases = []struct {
+	varLongCases = []struct {
 		bytes  []byte
-		value  position
+		value  int64
 		length int
 		err    error
 	}{
-		{[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, position{0, 0, 0}, 8, nil}, // zero value
-		{[]byte{0x00, 0x00, 0x40, 0x40, 0xab, 0xff, 0xfd, 0xff}, position{257, 42, -513}, 8, nil},
-		{[]byte{}, position{}, 0, errBufTooSmall},                                         // empty buffer
-		{[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, position{}, 7, errBufTooSmall}, // incomplete buffer
+		// valid
+		{[]byte{0x00}, 0, 1, nil},         // zero
+		{[]byte{0x01}, 1, 1, nil},         // one
+		{[]byte{0x7f}, 127, 1, nil},       // largest value of length 1
+		{[]byte{0x80, 0x01}, 128, 2, nil}, // smallest value of length 2
+		{[]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f}, math.MaxInt64, 9, nil},        // largest positive value
+		{[]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01}, -1, 10, nil},            // negative one
+		{[]byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01}, math.MinInt64, 10, nil}, // largest negative number
+		// invalid
+		{[]byte{}, 0, 0, errBufTooSmall},     // empty buffer
+		{[]byte{0xff}, 0, 1, errBufTooSmall}, // incomplete buffer
+		{[]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01}, 0, 10, errValueTooLarge}, // value overflow
+		{[]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, 0, 10, errValueTooLarge},       // incomplete buffer with value overflow
 	}
 )
+
+func TestGetVarInt(t *testing.T) {
+	for _, c := range varIntCases {
+		v, n, err := getVarInt(c.bytes)
+		if v != c.value || n != c.length || errors.Cause(err) != c.err {
+			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)",
+				c.bytes,
+				c.value, c.length, c.err,
+				v, n, err)
+		}
+	}
+}
+
+func TestGetVarLong(t *testing.T) {
+	for _, c := range varLongCases {
+		v, n, err := getVarLong(c.bytes)
+		if v != c.value || n != c.length || errors.Cause(err) != c.err {
+			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)",
+				c.bytes,
+				c.value, c.length, c.err,
+				v, n, err)
+		}
+	}
+}
+
+func TestPutVarInt(t *testing.T) {
+	buf := make([]byte, maxIntBytes)
+	for _, c := range varIntCases {
+		if c.err != nil {
+			continue // skip invalid cases
+		}
+		n, err := putVarInt(buf, c.value)
+		if n != c.length || errors.Cause(err) != c.err || bytes.Compare(buf[:n], c.bytes) != 0 {
+			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)",
+				c.value,
+				c.bytes, c.length, c.err,
+				buf[:n], n, err)
+		}
+	}
+}
+
+func TestPutVarLong(t *testing.T) {
+	buf := make([]byte, maxLongBytes)
+	for _, c := range varLongCases {
+		if c.err != nil {
+			continue // skip invalid cases
+		}
+		n, err := putVarLong(buf, c.value)
+		if n != c.length || errors.Cause(err) != c.err || bytes.Compare(buf[:n], c.bytes) != 0 {
+			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)",
+				c.value,
+				c.bytes, c.length, c.err,
+				buf[:n], n, err)
+		}
+	}
+}
+
+func TestLenVarInt(t *testing.T) {
+	for _, c := range varIntCases {
+		if c.err != nil {
+			continue // skip invalid cases
+		}
+		n := lenVarInt(c.value)
+		if n != c.length {
+			t.Errorf("have: %#v, want: %#v, got: %#v", c.value, c.length, n)
+		}
+	}
+}
+
+func TestLenVarLong(t *testing.T) {
+	for _, c := range varLongCases {
+		if c.err != nil {
+			continue // skip invalid cases
+		}
+		n := lenVarLong(c.value)
+		if n != c.length {
+			t.Errorf("have: %#v, want: %#v, got: %#v", c.value, c.length, n)
+		}
+	}
+}
+
+const (
+	benchmarkSeed   = 1
+	benchmarkDatums = 256
+)
+
+func BenchmarkGetVarInt(b *testing.B) {
+	rand.Seed(benchmarkSeed)
+	data := make([][]byte, benchmarkDatums)
+	for i := range data {
+		buf := make([]byte, maxIntBytes)
+		v := int32(rand.Uint32())
+		n, err := putVarInt(buf, v)
+		if err != nil {
+			b.Fatalf("failed to create benchmark data: have: %#v, got: (%#v, %#v, %#v)",
+				v,
+				buf[:n], n, err)
+		}
+		data[i] = buf[:n]
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		getVarInt(data[i%benchmarkDatums])
+	}
+}
+
+func BenchmarkGetVarLong(b *testing.B) {
+	rand.Seed(benchmarkSeed)
+	data := make([][]byte, benchmarkDatums)
+	for i := range data {
+		buf := make([]byte, maxLongBytes)
+		v := int64(rand.Uint64())
+		n, err := putVarLong(buf, v)
+		if err != nil {
+			b.Fatalf("failed to create benchmark data: have: %#v, got: (%#v, %#v, %#v)",
+				v,
+				buf[:n], n, err)
+		}
+		data[i] = buf[:n]
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		getVarLong(data[i%benchmarkDatums])
+	}
+}
+
+func BenchmarkPutVarInt(b *testing.B) {
+	rand.Seed(benchmarkSeed)
+	data := make([]int32, benchmarkDatums)
+	for i := range data {
+		data[i] = int32(rand.Uint32())
+	}
+	buf := make([]byte, maxIntBytes)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		putVarInt(buf, data[i%benchmarkDatums])
+	}
+}
+
+func BenchmarkPutVarLong(b *testing.B) {
+	rand.Seed(benchmarkSeed)
+	data := make([]int64, benchmarkDatums)
+	for i := range data {
+		data[i] = int64(rand.Uint64())
+	}
+	buf := make([]byte, maxLongBytes)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		putVarLong(buf, data[i%benchmarkDatums])
+	}
+}
+
+func BenchmarkLenVarInt(b *testing.B) {
+	rand.Seed(benchmarkSeed)
+	data := make([]int32, benchmarkDatums)
+	for i := range data {
+		data[i] = int32(rand.Uint32())
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		lenVarInt(data[i%benchmarkDatums])
+	}
+}
+
+func BenchmarkLenVarLong(b *testing.B) {
+	rand.Seed(benchmarkSeed)
+	data := make([]int64, benchmarkDatums)
+	for i := range data {
+		data[i] = int64(rand.Uint64())
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		lenVarLong(data[i%benchmarkDatums])
+	}
+}
+
+var stringCases = []struct {
+	bytes  []byte
+	value  string
+	length int
+	err    error
+}{
+	{[]byte{0x00}, "", 1, nil}, // empty string
+	{[]byte{0x0d, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21}, "Hello, world!", 14, nil},    // ASCII string
+	{[]byte{0x0e, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0xe4, 0xb8, 0x96, 0xe7, 0x95, 0x8c, 0x21}, "Hello, 世界!", 15, nil}, // UTF-8 string
+	{[]byte{}, "", 0, errBufTooSmall},                   // empty buffer
+	{[]byte{0x01}, "", 1, errBufTooSmall},               // incomplete buffer
+	{[]byte{0xff, 0xff, 0x02}, "", 3, errValueTooLarge}, // string length > math.MaxInt16
+}
 
 func TestGetString(t *testing.T) {
 	for _, c := range stringCases {
@@ -51,28 +264,6 @@ func TestPutString(t *testing.T) {
 		}
 		buf := make([]byte, c.length)
 		n, err := putString(buf, c.value)
-		if n != c.length || errors.Cause(err) != c.err || bytes.Compare(buf[:n], c.bytes) != 0 {
-			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)", c.value, c.length, c.err, c.bytes, n, err, buf[:n])
-		}
-	}
-}
-
-func TestGetPosition(t *testing.T) {
-	for _, c := range positionCases {
-		p, n, err := getPosition(c.bytes)
-		if p != c.value || n != c.length || errors.Cause(err) != c.err {
-			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)", c.bytes, c.value, c.length, c.err, p, n, err)
-		}
-	}
-}
-
-func TestPutPosition(t *testing.T) {
-	for _, c := range positionCases {
-		if c.err != nil {
-			continue // skip invalid cases
-		}
-		buf := make([]byte, c.length)
-		n, err := putPosition(buf, c.value)
 		if n != c.length || errors.Cause(err) != c.err || bytes.Compare(buf[:n], c.bytes) != 0 {
 			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)", c.value, c.length, c.err, c.bytes, n, err, buf[:n])
 		}
@@ -118,6 +309,40 @@ func BenchmarkPutString(b *testing.B) {
 	}
 }
 
+var positionCases = []struct {
+	bytes  []byte
+	value  position
+	length int
+	err    error
+}{
+	{[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, position{0, 0, 0}, 8, nil}, // zero value
+	{[]byte{0x00, 0x00, 0x40, 0x40, 0xab, 0xff, 0xfd, 0xff}, position{257, 42, -513}, 8, nil},
+	{[]byte{}, position{}, 0, errBufTooSmall},                                         // empty buffer
+	{[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, position{}, 7, errBufTooSmall}, // incomplete buffer
+}
+
+func TestGetPosition(t *testing.T) {
+	for _, c := range positionCases {
+		p, n, err := getPosition(c.bytes)
+		if p != c.value || n != c.length || errors.Cause(err) != c.err {
+			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)", c.bytes, c.value, c.length, c.err, p, n, err)
+		}
+	}
+}
+
+func TestPutPosition(t *testing.T) {
+	for _, c := range positionCases {
+		if c.err != nil {
+			continue // skip invalid cases
+		}
+		buf := make([]byte, c.length)
+		n, err := putPosition(buf, c.value)
+		if n != c.length || errors.Cause(err) != c.err || bytes.Compare(buf[:n], c.bytes) != 0 {
+			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)", c.value, c.length, c.err, c.bytes, n, err, buf[:n])
+		}
+	}
+}
+
 func BenchmarkGetPosition(b *testing.B) {
 	rand.Seed(benchmarkSeed)
 	data := make([][]byte, benchmarkDatums)
@@ -155,5 +380,93 @@ func BenchmarkPutPosition(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		putPosition(buf, data[i%benchmarkDatums])
+	}
+}
+
+var packetCases = []struct {
+	bytes  []byte
+	value  packet
+	length int
+	err    error
+}{
+	{[]byte{0x01, 0x00}, packet{id: 0x00, data: []byte{}}, 2, nil},
+	{[]byte{0x02, 0x00, 0x00}, packet{id: 0x00, data: []byte{0x00}}, 3, nil},
+	{[]byte{0x05, 0x04, 0x03, 0x02, 0x01, 0x00}, packet{id: 0x04, data: []byte{0x03, 0x02, 0x01, 0x00}}, 6, nil},
+	{[]byte{0x02, 0x80, 0x01}, packet{id: 0x80, data: []byte{}}, 3, nil},
+	{[]byte{0x80, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, packet{id: 0x00, data: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}, 130, nil},
+	{[]byte{}, packet{}, 0, errBufTooSmall},     // empty buffer
+	{[]byte{0x01}, packet{}, 1, errBufTooSmall}, // incomplete buffer
+}
+
+func TestGetPacket(t *testing.T) {
+	for _, c := range packetCases {
+		p, n, err := getPacket(c.bytes)
+		if !reflect.DeepEqual(p, c.value) || n != c.length || errors.Cause(err) != c.err {
+			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)", c.bytes, c.value, c.length, c.err, p, n, err)
+		}
+	}
+}
+
+func TestPutPacket(t *testing.T) {
+	for _, c := range packetCases {
+		if c.err != nil {
+			continue // skip invalid cases
+		}
+		buf := make([]byte, c.length)
+		n, err := putPacket(buf, c.value)
+		if n != c.length || errors.Cause(err) != c.err || bytes.Compare(buf[:n], c.bytes) != 0 {
+			t.Errorf("have: %#v, want: (%#v, %#v, %#v), got: (%#v, %#v, %#v)", c.value, c.length, c.err, c.bytes, n, err, buf[:n])
+		}
+	}
+}
+
+const (
+	benchmarkMaxPacketID      = 256
+	benchmarkMaxPacketDataLen = 512
+)
+
+func BenchmarkGetPacket(b *testing.B) {
+	rand.Seed(benchmarkSeed)
+	data := make([][]byte, benchmarkDatums)
+	for i := range data {
+		p := packet{
+			id:   int32(rand.Intn(benchmarkMaxPacketID)),
+			data: make([]byte, rand.Intn(benchmarkMaxPacketDataLen)),
+		}
+		rand.Read(p.data)
+
+		buf := make([]byte, 2*maxIntBytes+benchmarkMaxPacketDataLen)
+		n, err := putPacket(buf, p)
+		if err != nil {
+			b.Fatalf("failed to create benchmark data: have: %#v, got: (%#v, %#v, %#v)",
+				p,
+				n, err, buf[:n])
+		}
+
+		data[i] = buf[:n]
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		getPacket(data[i%benchmarkDatums])
+	}
+}
+
+func BenchmarkPutPacket(b *testing.B) {
+	rand.Seed(benchmarkSeed)
+	data := make([]packet, benchmarkDatums)
+	for i := range data {
+		p := packet{
+			id:   int32(rand.Intn(benchmarkMaxPacketID)),
+			data: make([]byte, rand.Intn(benchmarkMaxPacketDataLen)),
+		}
+		rand.Read(p.data)
+		data[i] = p
+	}
+	buf := make([]byte, 2*maxIntBytes+benchmarkMaxPacketDataLen)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		putPacket(buf, data[i%benchmarkDatums])
 	}
 }
