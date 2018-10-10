893dd10a0b2f2c1d753971e8f4204d3f161b1be8
njhanley <njhanley@users.noreply.github.com>
2018-10-10 16:24:09 -0400

    add tests for positions
    close #2

--- types_test.go
+++ types_test.go
@@ -21,6 +21,17 @@ var (
 		{[]byte{0x01}, "", 1, errBufTooSmall},               // incomplete buffer
 		{[]byte{0xff, 0xff, 0x02}, "", 3, errValueTooLarge}, // string length > math.MaxInt16
 	}
+	positionCases = []struct {
+		bytes  []byte
+		value  position
+		length int
+		err    error
+	}{
+		{[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, position{0, 0, 0}, 8, nil}, // zero value
+		{[]byte{0x00, 0x00, 0x40, 0x40, 0xab, 0xff, 0xfd, 0xff}, position{257, 42, -513}, 8, nil},
+		{[]byte{}, position{}, 0, errBufTooSmall},                                         // empty buffer
+		{[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, position{}, 7, errBufTooSmall}, // incomplete buffer
+	}
 )
 
 func TestGetString(t *testing.T) {
@@ -41,6 +52,28 @@ func TestPutString(t *testing.T) {
 		n, err := putString(buf, c.value)
 		if n != c.length || errors.Cause(err) != c.err || bytes.Compare(buf[:n], c.bytes) != 0 {
 			t.Errorf("have: %q, want: (%d, %v, [% #x]), got: (%d, %v, [% #x])", c.value, c.length, c.err, c.bytes, n, err, buf[:n])
+		}
+	}
+}
+
+func TestGetPosition(t *testing.T) {
+	for _, c := range positionCases {
+		p, n, err := getPosition(c.bytes)
+		if p != c.value || n != c.length || errors.Cause(err) != c.err {
+			t.Errorf("have: %v, want: (%v, %v, %v), got: (%v, %v, %v)", c.bytes, c.value, c.length, c.err, p, n, err)
+		}
+	}
+}
+
+func TestPutPosition(t *testing.T) {
+	for _, c := range positionCases {
+		if c.err != nil {
+			continue // skip invalid cases
+		}
+		buf := make([]byte, c.length)
+		n, err := putPosition(buf, c.value)
+		if n != c.length || errors.Cause(err) != c.err || bytes.Compare(buf[:n], c.bytes) != 0 {
+			t.Errorf("have: %v, want: (%v, %v, %v), got: (%v, %v, %v)", c.value, c.length, c.err, c.bytes, n, err, buf[:n])
 		}
 	}
 }
